5ccf5ba5684d423a90922aee543b558cf4452435
kat1906 <katie.armstrong@bjss.com>
2019-11-21 16:58:06 +0000

    add tag component

---
+++ app/components/tag/macro.test.js
@@ -0,0 +1,71 @@
+import request from 'supertest';
+import express from 'express';
+import nunjucks from 'nunjucks';
+import cheerio from 'cheerio';
+import { App } from '../../../app';
+
+const createDummyApp = (context) => {
+  const app = new App().createApp();
+
+  const router = express.Router();
+  const dummyRouter = router.get('/', (req, res) => {
+    const macroWrapper = `{% from './components/tag/macro.njk' import tag %}
+                            {{ tag({ qaIdentifier, text, classes }) }}`;
+
+    const viewToTest = nunjucks.renderString(macroWrapper, context);
+
+    res.send(viewToTest);
+  });
+
+  app.use(dummyRouter);
+
+  return app;
+};
+
+describe('tag', () => {
+  it('should render the tag with the correct data-test-id', (done) => {
+    const context = {
+      qaIdentifier: 'qa-identifier',
+    };
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+        expect($('div[data-test-id="qa-identifier-tag"]').length).toEqual(1);
+        done();
+      });
+  });
+
+  it('should render the tag with the correct text', (done) => {
+    const context = {
+      qaIdentifier: 'qa-identifier',
+      text: 'some tag text',
+    };
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+        expect($('div[data-test-id="qa-identifier-tag"]').text().trim()).toEqual('some tag text');
+        done();
+      });
+  });
+
+  it('should render the tag with the correct classes', (done) => {
+    const context = {
+      qaIdentifier: 'qa-identifier',
+      text: 'some tag text',
+      classes: 'extra-class',
+    };
+    const dummyApp = createDummyApp(context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+        expect($('div[data-test-id="qa-identifier-tag"]').hasClass('bc-c-tag')).toEqual(true);
+        expect($('div[data-test-id="qa-identifier-tag"]').hasClass('extra-class')).toEqual(true);
+        done();
+      });
+  });
+});

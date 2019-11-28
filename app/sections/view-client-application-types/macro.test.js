95a85e29449bc3e96d757004693a687d2d34618f
alibombaye <alibombaye@gmail.com>
2019-11-28 16:41:33 +0000

    add client-application-types section

--- app/sections/view-client-application-types/macro.test.js
+++ app/sections/view-client-application-types/macro.test.js
@@ -1,34 +1,19 @@
 import request from 'supertest';
-import express from 'express';
-import nunjucks from 'nunjucks';
 import cheerio from 'cheerio';
-import { App } from '../../../app';
+import { createTestHarness } from '../../testUtils/testHarness';
 
-const createDummyApp = (context) => {
-  const app = new App().createApp();
+const macroWrapper = `{% from './sections/view-client-application-types/macro.njk' import viewClientApplicationTypes %}
+                        {{ viewClientApplicationTypes(params) }}`;
 
-  const router = express.Router();
-  const dummyRouter = router.get('/', (req, res) => {
-    const macroWrapper = `{% from './preview/sections/preview-client-application-types.njk' import previewClientApplicationTypes %}
-                            {{ previewClientApplicationTypes(section) }}`;
-
-    const viewToTest = nunjucks.renderString(macroWrapper, context);
-
-    res.send(viewToTest);
-  });
-
-  app.use(dummyRouter);
-
-  return app;
-};
-
-describe('preview-client-application-types', () => {
+describe('view-client-application-types', () => {
   it('should render the title of the section', (done) => {
     const context = {
-      section: {},
+      params: {
+        section: {},
+      },
     };
 
-    const dummyApp = createDummyApp(context);
+    const dummyApp = createTestHarness(macroWrapper, context);
     request(dummyApp)
       .get('/')
       .then((res) => {
@@ -42,15 +27,16 @@ describe('preview-client-application-types', () => {
 
   it('should not render the client-application-types section when not provided', (done) => {
     const context = {
+      params: {},
     };
 
-    const dummyApp = createDummyApp(context);
+    const dummyApp = createTestHarness(macroWrapper, context);
     request(dummyApp)
       .get('/')
       .then((res) => {
         const $ = cheerio.load(res.text);
 
-        expect($('[data-test-id="preview-client-application-types"]').length).toEqual(0);
+        expect($('[data-test-id="view-client-application-types"]').length).toEqual(0);
 
         done();
       });
@@ -59,22 +45,24 @@ describe('preview-client-application-types', () => {
   describe('when a sub section exists for an application type', () => {
     it('should render the browsed based application type', (done) => {
       const context = {
-        section: {
-          sections: {
-            'browser-based': {},
+        params: {
+          section: {
+            sections: {
+              'browser-based': {},
+            },
           },
         },
       };
 
-      const dummyApp = createDummyApp(context);
+      const dummyApp = createTestHarness(macroWrapper, context);
       request(dummyApp)
         .get('/')
         .then((res) => {
           const $ = cheerio.load(res.text);
 
 
-          const browserBasedExpandableSection = $('[data-test-id="preview-section-browser-based"]');
-          const browserBasedSection = browserBasedExpandableSection.find('[data-test-id="preview-section-table-browser-based"]');
+          const browserBasedExpandableSection = $('[data-test-id="view-section-browser-based"]');
+          const browserBasedSection = browserBasedExpandableSection.find('[data-test-id="view-section-table-browser-based"]');
 
           expect(browserBasedExpandableSection.length).toEqual(1);
           expect(browserBasedSection.length).toEqual(1);
@@ -87,23 +75,25 @@ describe('preview-client-application-types', () => {
   describe('when a sub section does not exist for an application type', () => {
     it('should not render the browsed based application type', (done) => {
       const context = {
-        section: {
-          sections: {
-            'some-other-section': {
-              answers: {
-                'some-question': 'Some data',
+        params: {
+          section: {
+            sections: {
+              'some-other-section': {
+                answers: {
+                  'some-question': 'Some data',
+                },
               },
             },
           },
         },
       };
 
-      const dummyApp = createDummyApp(context);
+      const dummyApp = createTestHarness(macroWrapper, context);
       request(dummyApp)
         .get('/')
         .then((res) => {
           const $ = cheerio.load(res.text);
-          const browserBasedExpandableSection = $('[data-test-id="preview-section-browser-based"]');
+          const browserBasedExpandableSection = $('[data-test-id="view-section-browser-based"]');
 
           expect(browserBasedExpandableSection.length).toEqual(0);
 

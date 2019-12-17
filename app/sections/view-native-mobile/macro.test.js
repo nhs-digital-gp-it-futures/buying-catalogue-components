d03ec025a74f4f6611950d7b3f2cdb37baadad6a
Ben.Hayward <ben.hayward@bjss.com>
2019-12-17 16:47:06 +0000

    Add connection type component

--- app/sections/view-native-mobile/macro.test.js
+++ app/sections/view-native-mobile/macro.test.js
@@ -29,7 +29,7 @@ describe('view-native-mobile', () => {
 
         const nativeMobileSectionTable = $('[data-test-id="view-section-table-native-mobile"]');
         const operatingSystemsQuestionRow = nativeMobileSectionTable.find('[data-test-id="view-section-table-row-operating-systems"]');
-        const supportedOperatingSystemsInnerComponent = operatingSystemsQuestionRow
+        const operatingSystemsInnerComponent = operatingSystemsQuestionRow
           .find('div[data-test-id="view-section-table-row-component"]')
           .find('[data-test-id="view-question-data-bulletlist"]');
 
@@ -37,8 +37,8 @@ describe('view-native-mobile', () => {
         expect(operatingSystemsQuestionRow.length).toEqual(1);
         expect(operatingSystemsQuestionRow
           .find('div[data-test-id="view-section-table-row-title"]').text().trim()).toEqual('Supported operating systems');
-        expect(supportedOperatingSystemsInnerComponent.length).toEqual(1);
-        expect(supportedOperatingSystemsInnerComponent.text().trim()).toEqual('Apple IOS');
+        expect(operatingSystemsInnerComponent.length).toEqual(1);
+        expect(operatingSystemsInnerComponent.text().trim()).toEqual('Apple IOS');
 
         done();
       });
@@ -115,6 +115,120 @@ describe('view-native-mobile', () => {
           .find('div[data-test-id="view-section-table-row-title"]').text().trim()).toEqual('Designed with a mobile first approach');
         expect(mobileFirstDesignInnerComponent.length).toEqual(1);
         expect(mobileFirstDesignInnerComponent.text().trim()).toEqual('Yes');
+
+        done();
+      });
+  });
+
+  it('should render the minimum connection speed required answer', (done) => {
+    const context = {
+      params: {
+        section: {
+          sections: {
+            'mobile-connection-details': {
+              answers: {
+                'minimum-connection-speed': '1Mbps',
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const dummyApp = createTestHarness(macroWrapper, context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        const nativeMobileSectionTable = $('[data-test-id="view-section-table-native-mobile"]');
+        const minimumConnectionSpeedQuestionRow = nativeMobileSectionTable.find('[data-test-id="view-section-table-row-minimum-connection-speed"]');
+        const minimumConnectionSpeedInnerComponent = minimumConnectionSpeedQuestionRow
+          .find('div[data-test-id="view-section-table-row-component"]')
+          .find('[data-test-id="view-question-data-text-minimum-connection-speed"]');
+
+        expect(nativeMobileSectionTable.length).toEqual(1);
+        expect(minimumConnectionSpeedQuestionRow.length).toEqual(1);
+        expect(minimumConnectionSpeedQuestionRow
+          .find('div[data-test-id="view-section-table-row-title"]').text().trim()).toEqual('Minimum connection speed required');
+        expect(minimumConnectionSpeedInnerComponent.length).toEqual(1);
+        expect(minimumConnectionSpeedInnerComponent.text().trim()).toEqual('1Mbps');
+
+        done();
+      });
+  });
+
+  it('should render the connection types supported answer', (done) => {
+    const context = {
+      params: {
+        section: {
+          sections: {
+            'mobile-connection-details': {
+              answers: {
+                'connection-types': ['GPRS'],
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const dummyApp = createTestHarness(macroWrapper, context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        const nativeMobileSectionTable = $('[data-test-id="view-section-table-native-mobile"]');
+        const connectionTypesQuestionRow = nativeMobileSectionTable.find('[data-test-id="view-section-table-row-connection-types"]');
+        const connectionTypesInnerComponent = connectionTypesQuestionRow
+          .find('div[data-test-id="view-section-table-row-component"]')
+          .find('[data-test-id="view-question-data-bulletlist"]');
+
+        expect(nativeMobileSectionTable.length).toEqual(1);
+        expect(connectionTypesQuestionRow.length).toEqual(1);
+        expect(connectionTypesQuestionRow
+          .find('div[data-test-id="view-section-table-row-title"]').text().trim()).toEqual('Connection types supported');
+        expect(connectionTypesInnerComponent.length).toEqual(1);
+        expect(connectionTypesInnerComponent.text().trim()).toEqual('GPRS');
+
+        done();
+      });
+  });
+
+  it('should render the additional information about connection types answer', (done) => {
+    const context = {
+      params: {
+        section: {
+          sections: {
+            'mobile-connection-details': {
+              answers: {
+                'connection-requirements-description': 'Average data usage will vary depending on application activity.'
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const dummyApp = createTestHarness(macroWrapper, context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        const nativeMobileSectionTable = $('[data-test-id="view-section-table-native-mobile"]');
+        const connectionRequirementsDescriptionQuestionRow = nativeMobileSectionTable.find('[data-test-id="view-section-table-row-connection-requirements-description"]');
+        const connectionRequirementsDescriptionInnerComponent = connectionRequirementsDescriptionQuestionRow
+          .find('div[data-test-id="view-section-table-row-component"]')
+          .find('[data-test-id="view-question-data-text-connection-requirements-description"]');
+
+        expect(nativeMobileSectionTable.length).toEqual(1);
+        expect(connectionRequirementsDescriptionQuestionRow.length).toEqual(1);
+        expect(connectionRequirementsDescriptionQuestionRow
+          .find('div[data-test-id="view-section-table-row-title"]').text().trim()).toEqual('Additional information about connection types');
+        expect(connectionRequirementsDescriptionInnerComponent.length).toEqual(1);
+        expect(connectionRequirementsDescriptionInnerComponent.text().trim()).toEqual('Average data usage will vary depending on application activity.');
 
         done();
       });

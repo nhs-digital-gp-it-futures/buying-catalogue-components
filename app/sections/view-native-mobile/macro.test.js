dfa136a989e2610fb5ad9216c893115b817e8f62
Ben.Hayward <ben.hayward@bjss.com>
2019-12-17 15:04:25 +0000

    Add operating systems component

--- app/sections/view-native-mobile/macro.test.js
+++ app/sections/view-native-mobile/macro.test.js
@@ -6,6 +6,79 @@ const macroWrapper = `{% from './sections/view-native-mobile/macro.njk' import v
                         {{ viewNativeMobile(params) }}`;
 
 describe('view-native-mobile', () => {
+  it('should render the supported operating systems answer', (done) => {
+    const context = {
+      params: {
+        section: {
+          sections: {
+            'mobile-operating-systems': {
+              answers: {
+                'operating-systems': ['Apple IOS'],
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const dummyApp = createTestHarness(macroWrapper, context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        const nativeMobileSectionTable = $('[data-test-id="view-section-table-native-mobile"]');
+        const operatingSystemsQuestionRow = nativeMobileSectionTable.find('[data-test-id="view-section-table-row-operating-systems"]');
+        const supportedOperatingSystemsInnerComponent = operatingSystemsQuestionRow
+          .find('div[data-test-id="view-section-table-row-component"]')
+          .find('[data-test-id="view-question-data-bulletlist"]');
+
+        expect(nativeMobileSectionTable.length).toEqual(1);
+        expect(operatingSystemsQuestionRow.length).toEqual(1);
+        expect(operatingSystemsQuestionRow
+          .find('div[data-test-id="view-section-table-row-title"]').text().trim()).toEqual('Supported operating systems');
+        expect(supportedOperatingSystemsInnerComponent.length).toEqual(1);
+        expect(supportedOperatingSystemsInnerComponent.text().trim()).toEqual('Apple IOS');
+
+        done();
+      });
+  });
+
+  it('should render the additional operating system information answer', (done) => {
+    const context = {
+      params: {
+        section: {
+          sections: {
+            'mobile-operating-systems': {
+              answers: {
+                'operating-systems-description': 'Android 4.1 and above, IOS 10.6 and above.',
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const dummyApp = createTestHarness(macroWrapper, context);
+    request(dummyApp)
+      .get('/')
+      .then((res) => {
+        const $ = cheerio.load(res.text);
+
+        const nativeMobileSectionTable = $('[data-test-id="view-section-table-native-mobile"]');
+        const operatingSystemsDescriptionQuestionRow = nativeMobileSectionTable.find('[data-test-id="view-section-table-row-operating-systems-description"]');
+
+        expect(nativeMobileSectionTable.length).toEqual(1);
+        expect(operatingSystemsDescriptionQuestionRow.length).toEqual(1);
+        expect(operatingSystemsDescriptionQuestionRow
+          .find('div[data-test-id="view-section-table-row-title"]').text().trim()).toEqual('Additional operating system information');
+        expect(operatingSystemsDescriptionQuestionRow
+          .find('div[data-test-id="view-section-table-row-component"]').length).toEqual(1);
+
+        done();
+      });
+  });
+
   it('should render the mobile first answer', (done) => {
     const context = {
       params: {
@@ -28,13 +101,13 @@ describe('view-native-mobile', () => {
         const $ = cheerio.load(res.text);
 
         const nativeMobileSectionTable = $('[data-test-id="view-section-table-native-mobile"]');
-        const nativeMobileQuestionRow = nativeMobileSectionTable.find('[data-test-id="view-section-table-row-mobile-first-design"]');
+        const mobileFirstDesignQuestionRow = nativeMobileSectionTable.find('[data-test-id="view-section-table-row-mobile-first-design"]');
 
         expect(nativeMobileSectionTable.length).toEqual(1);
-        expect(nativeMobileQuestionRow.length).toEqual(1);
-        expect(nativeMobileQuestionRow
+        expect(mobileFirstDesignQuestionRow.length).toEqual(1);
+        expect(mobileFirstDesignQuestionRow
           .find('div[data-test-id="view-section-table-row-title"]').text().trim()).toEqual('Designed with a mobile first approach');
-        expect(nativeMobileQuestionRow
+        expect(mobileFirstDesignQuestionRow
           .find('div[data-test-id="view-section-table-row-component"]').length).toEqual(1);
 
         done();

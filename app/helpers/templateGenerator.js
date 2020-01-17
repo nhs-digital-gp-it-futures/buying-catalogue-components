8c6af6eb9abb4a1606ea2f95d1d3cdaac36a3210
kat1906 <katie.armstrong@bjss.com>
2020-01-17 11:56:15 +0000

    add in the logic from the routes and imporvements to the template

--- app/helpers/templateGenerator.js
+++ app/helpers/templateGenerator.js
@@ -3,29 +3,41 @@ const fs = require('fs');
 const getInputFields = (params) => {
   const paramsInputString = Object.entries(params).map(([key, value]) => {
     let modifiedValue;
-    if (value.includes('&lt') || value.includes('&gt')) {
+    if (value.includes('&lt') || value.includes('&gt;')) {
       modifiedValue = value.replace(/&lt/g, '<').replace(/&gt;/g, '>');
     } else modifiedValue = value;
     return `
       <div class="nhsuk-u-inline-block nhsuk-u-padding-4 nhsuk-u-padding-bottom-0">
         <label for="param" class="nhsuk-grid-column-one-quarter">${key}:</label>
-        <input type="text" name="${key}" id="${key}" value="${modifiedValue}" class="nhsuk-grid-column-three-quarters nhsuk-u-font-size-16" style="line-height: 2em;"/>
+        <input type="text" name="${key}" id="${key}" value="${modifiedValue}" class="nhsuk-grid-column-three-quarters nhsuk-u-font-size-16"/>
       </div>
     `;
   }).join('');
   return paramsInputString;
 };
 
+const getSettings = (component) => {
+  const settingsString = fs.readFileSync(`app/components/${component}/settings.json`, 'utf-8');
+  return JSON.parse(settingsString);
+};
+
 const generateTemplate = ({
   component,
-  componentName,
-  params,
-  type,
+  formParams = {},
 }) => {
+  const settings = getSettings(component);
+  const { componentName } = settings;
+  const type = 'component';
+  const formParamsExist = Object.keys(formParams).length > 0;
+
+  const params = formParamsExist ? formParams : settings.params;
+
   const paramsStringCode = Object.entries(params).reduce((acc, [key, value], i) => {
     let modifiedValue;
     if (value.includes('<') || value.includes('>')) {
       modifiedValue = value.replace(/</g, '&lt').replace(/>/g, '&gt;');
+    } else if (component === 'view-data-bulletlist' && key === 'data' && (typeof value === 'string')) {
+      modifiedValue = value.split(',');
     } else modifiedValue = value;
     const keyValueString = (i !== 0)
       ? (`,\n        ${key}: ${JSON.stringify(modifiedValue)}`)
@@ -36,8 +48,10 @@ const generateTemplate = ({
 
   const paramsString = Object.entries(params).reduce((acc, [key, value], i) => {
     let modifiedValue;
-    if (value.includes('&lt') || value.includes('&gt')) {
+    if (value.includes('&lt') || value.includes('&gt;')) {
       modifiedValue = value.replace(/&lt/g, '<').replace(/&gt;/g, '>');
+    } else if (component === 'view-data-bulletlist' && key === 'data' && (typeof value === 'string')) {
+      modifiedValue = value.split(',');
     } else modifiedValue = value;
     const keyValueString = (i !== 0)
       ? `, ${key}: ${JSON.stringify(modifiedValue)}`
@@ -71,19 +85,22 @@ const generateTemplate = ({
   </code></pre>
 
   <h3>Rendered ${type}</h3>
-  <div style="background:white;">
+  <div class="bcc-t-bg-white">
   ${renderedComponent}
   </div>
 
-  <div class="nhsuk-grid-row nhsuk-u-padding-4 nhsuk-u-margin-top-4 nhsuk-u-margin-left-0" style="background:#fff9c4;">
-    <form method="post" action="/${type}/${component}" id="try-params" class="bc-u-clearfix">
+  <div class="nhsuk-grid-row nhsuk-u-padding-4 nhsuk-u-margin-top-4 nhsuk-u-margin-left-0 bcc-t-bg-pale-yellow">
+    <form method="post" action="/${type}/${component}" id="try-params" class="nhsuk-u-clear">
     ${getInputFields(params)}
-    <button type="submit" form="try-params" class="nhsuk-u-margin-top-4 nhsuk-u-margin-right-4 nhsuk-u-font-size-16" style="float:right;background-color:#007f3b;color:#fff;box-shadow:0 4px 0 #00401e;padding:6px 8px;box-sizing:border-box;border: 2px solid transparent;border-radius: 4px;font-weight: 600;">Try it out</button>
+    <button type="submit" form="try-params" class="nhsuk-u-margin-top-4 nhsuk-u-margin-right-4 nhsuk-u-font-size-16 bcc-c-try-button">Try it out</button>
     </form>
   </div>
 {% endblock %}
 `;
-  fs.writeFileSync('app/template.njk', template);
+  const dir = 'app/templates';
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+  fs.writeFileSync(`${dir}/${component}-template.njk`, template);
 };
 
 module.exports = {

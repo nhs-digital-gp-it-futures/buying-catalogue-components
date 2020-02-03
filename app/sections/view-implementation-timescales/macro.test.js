ffd2f6b72f84efb1d573d2b22d2af592369bbb79
Ben.Hayward <ben.hayward@bjss.com>
2020-02-03 11:30:43 +0000

    Test context from settings.json

--- app/sections/view-implementation-timescales/macro.test.js
+++ app/sections/view-implementation-timescales/macro.test.js
@@ -2,20 +2,14 @@ import request from 'supertest';
 import cheerio from 'cheerio';
 import { createTestHarness } from '../../testUtils/testHarness';
 
+const settingsContext = require('./settings.json');
+
 const macroWrapper = `{% from './sections/view-implementation-timescales/macro.njk' import viewImplementationTimescales %}
                         {{ viewImplementationTimescales(params) }}`;
 
 describe('view-implementation-timescales', () => {
   it('should render the title of the section', (done) => {
-    const context = {
-      params: {
-        section: {
-          answers: {
-            description: 'Implementations without transition from another Catalogue Solution typically take 3-5 working days, the average is 4. The extent of your configuration requirements will have the greatest impact on these timescales. Your main responsibility will be configuration planning and approval.',
-          },
-        },
-      },
-    };
+    const context = settingsContext;
 
     const dummyApp = createTestHarness(macroWrapper, context);
     request(dummyApp)
@@ -28,15 +22,7 @@ describe('view-implementation-timescales', () => {
   });
 
   it('should render the description answer when provided', (done) => {
-    const context = {
-      params: {
-        section: {
-          answers: {
-            description: 'Implementations without transition from another Catalogue Solution typically take 3-5 working days, the average is 4. The extent of your configuration requirements will have the greatest impact on these timescales. Your main responsibility will be configuration planning and approval.',
-          },
-        },
-      },
-    };
+    const context = settingsContext;
 
     const dummyApp = createTestHarness(macroWrapper, context);
     request(dummyApp)
@@ -44,7 +30,7 @@ describe('view-implementation-timescales', () => {
       .then((res) => {
         const $ = cheerio.load(res.text);
 
-        expect($('[data-test-id="view-question-data-text-description"]').text().trim()).toEqual('Implementations without transition from another Catalogue Solution typically take 3-5 working days, the average is 4. The extent of your configuration requirements will have the greatest impact on these timescales. Your main responsibility will be configuration planning and approval.');
+        expect($('[data-test-id="view-question-data-text-description"]').text().trim()).toEqual(context.params.section.answers.description);
 
         done();
       });

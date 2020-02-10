2b8e22a6b03a49ff9eb1342c7184c76546f74d6b
Ben.Hayward <ben.hayward@bjss.com>
2020-02-10 14:17:00 +0000

    Solution header tests

--- app/sections/view-solution-header/macro.test.js
+++ app/sections/view-solution-header/macro.test.js
@@ -16,7 +16,7 @@ const context = {
 };
 
 describe('view-solution-header', () => {
-  it('should not render the foundation tag if set to false', createTestHarness(setup, (harness) => {
+  it('should not render the foundation tag if provided and set to false', createTestHarness(setup, (harness) => {
     const newContext = {
       params: {
         ...context,
@@ -29,33 +29,78 @@ describe('view-solution-header', () => {
     });
   }));
 
-  it('should render the foundation tag if set to true', createTestHarness(setup, (harness) => {
+  it('should render the foundation tag if provided and set to true', createTestHarness(setup, (harness) => {
     harness.request(context, ($) => {
       expect($('[data-test-id="view-solution-foundation"]').text().trim()).toEqual('Foundation Solution Set');
     });
   }));
 
-  it('should render the solution name', createTestHarness(setup, (harness) => {
+  it('should not render the foundation tag if not provided', createTestHarness(setup, (harness) => {
+    const newContext = { ...context };
+    delete newContext.params.isFoundation;
+
+    harness.request(newContext, ($) => {
+      expect($('[data-test-id="view-solution-foundation"]').length).toEqual(0);
+    });
+  }));
+
+  it('should render the solution name if provided', createTestHarness(setup, (harness) => {
     harness.request(context, ($) => {
       expect($('[data-test-id="view-solution-page-solution-name"]').text().trim()).toEqual('Write on Time');
     });
   }));
 
-  it('should render the supplier name', createTestHarness(setup, (harness) => {
+  it('should not render the solution name if not provided', createTestHarness(setup, (harness) => {
+    const newContext = { ...context };
+    delete newContext.params.name;
+
+    harness.request(newContext, ($) => {
+      expect($('[data-test-id="view-solution-page-solution-name"]').length).toEqual(0);
+    });
+  }));
+
+  it('should render the supplier name if provided', createTestHarness(setup, (harness) => {
     harness.request(context, ($) => {
       expect($('[data-test-id="view-solution-page-supplier-name"]').text().trim()).toEqual('Aperture Science');
     });
   }));
 
-  it('should render the solution id', createTestHarness(setup, (harness) => {
+  it('should not render the supplier name if not provided', createTestHarness(setup, (harness) => {
+    const newContext = { ...context };
+    delete newContext.params.supplierName;
+
+    harness.request(newContext, ($) => {
+      expect($('[data-test-id="view-solution-page-supplier-name"]').length).toEqual(0);
+    });
+  }));
+
+  it('should render the solution id if provided', createTestHarness(setup, (harness) => {
     harness.request(context, ($) => {
       expect($('[data-test-id="view-solution-page-solution-id"]').text().trim()).toEqual('Solution ID: 100000-001');
     });
   }));
 
-  it('should render the last updated date', createTestHarness(setup, (harness) => {
+  it('should not render the solution id if not provided', createTestHarness(setup, (harness) => {
+    const newContext = { ...context };
+    delete newContext.params.id;
+
+    harness.request(newContext, ($) => {
+      expect($('[data-test-id="view-solution-page-solution-id"]').length).toEqual(0);
+    });
+  }));
+
+  it('should render the last updated date if provided', createTestHarness(setup, (harness) => {
     harness.request(context, ($) => {
       expect($('[data-test-id="view-solution-page-last-updated"]').text().trim()).toEqual('Solution information last updated: 15 March 1996');
+    });
+  }));
+
+  it('should not render the last updated date if not provided', createTestHarness(setup, (harness) => {
+    const newContext = { ...context };
+    delete newContext.params.lastUpdated;
+
+    harness.request(newContext, ($) => {
+      expect($('[data-test-id="view-solution-page-last-updated"]').length).toEqual(0);
     });
   }));
 

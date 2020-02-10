fd466c8a1289b16a5f7887ef5fe59a85a6f8294a
Ben.Hayward <ben.hayward@bjss.com>
2020-02-10 13:40:17 +0000

    Add solution header section

---
+++ app/sections/view-solution-header/macro.test.js
@@ -0,0 +1,54 @@
+import { createTestHarness } from '../../testUtils/testHarness';
+
+const setup = {
+  templateName: 'viewSolutionHeader',
+  templateType: 'section',
+};
+
+const context = {
+  params: {
+    id: '100000-001',
+    name: 'Write on Time',
+    supplierName: 'Aperture Science',
+    isFoundation: true,
+    lastUpdated: '1996-03-15T10:00:00',
+  },
+};
+
+describe('view-solution-header', () => {
+  it('should render the foundation tag', createTestHarness(setup, (harness) => {
+    harness.request(context, ($) => {
+      expect($('[data-test-id="view-solution-foundation"]').text().trim()).toEqual('Foundation Solution Set');
+    });
+  }));
+
+  it('should render the solution name', createTestHarness(setup, (harness) => {
+    harness.request(context, ($) => {
+      expect($('[data-test-id="view-solution-page-solution-name"]').text().trim()).toEqual('Write on Time');
+    });
+  }));
+
+  it('should render the supplier name', createTestHarness(setup, (harness) => {
+    harness.request(context, ($) => {
+      expect($('[data-test-id="view-solution-page-supplier-name"]').text().trim()).toEqual('Aperture Science');
+    });
+  }));
+
+  it('should render the solution id', createTestHarness(setup, (harness) => {
+    harness.request(context, ($) => {
+      expect($('[data-test-id="view-solution-page-solution-id"]').text().trim()).toEqual('Solution ID: 100000-001');
+    });
+  }));
+
+  it('should render the last updated date', createTestHarness(setup, (harness) => {
+    harness.request(context, ($) => {
+      expect($('[data-test-id="view-solution-page-last-updated"]').text().trim()).toEqual('Solution information last updated: 15 March 1996');
+    });
+  }));
+
+  it('should not render the component if no data provided', createTestHarness(setup, (harness) => {
+    harness.request({}, ($) => {
+      expect($('[data-test-id="view-solution-header"]').length).toEqual(0);
+    });
+  }));
+});

f44a65c27736a37ebee36610f25cf10fdaf4a4b1
kat1906 <katie.armstrong@bjss.com>
2020-05-06 12:55:11 +0100

    add betsBanner

---
+++ app/components/general/components/bc-header/components/beta-banner.test.js
@@ -0,0 +1,31 @@
+import { componentTester } from '../../../../../testUtils/componentTester';
+
+const setup = {
+  component: {
+    name: 'betaBanner',
+    path: 'components/general/components/bc-header/components/beta-banner.njk',
+  },
+};
+
+
+describe('beta-banner', () => {
+  it('should render the betaBanner tag', componentTester(setup, (harness) => {
+    const context = {};
+
+    harness.request(context, ($) => {
+      expect($('div[data-test-id="beta-tag"]').length).toEqual(1);
+      expect($('div[data-test-id="beta-tag"]').text().trim()).toEqual('BETA');
+      expect($('div[data-test-id="beta-tag"]').hasClass('bc-c-tag-beta')).toBeTruthy();
+    });
+  }));
+
+  it('should render the betaBanner text', componentTester(setup, (harness) => {
+    const context = {};
+
+    harness.request(context, ($) => {
+      expect($('div[data-test-id="beta-banner-text"]').text().trim()).toEqual('By using this site you are accepting the General Terms of Use which you can view by downloading this PDF. The Cookies Policy and Privacy Policy can be accessed using the links at the bottom of the page.');
+      expect($('div[data-test-id="beta-banner-text"] a').text().trim()).toEqual('downloading this PDF');
+      expect($('div[data-test-id="beta-banner-text"] a').attr('href')).toEqual('/$web/content/terms-of-use.pdf');
+    });
+  }));
+});

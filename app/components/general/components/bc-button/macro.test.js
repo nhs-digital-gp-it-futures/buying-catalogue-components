d906f39ad50b1c7b34effe52124ebcee74d3a11e
Fawad Ali <alibombaye@gmail.com>
2020-05-07 13:48:09 +0100

    added tests for bcButton component

--- app/components/general/components/bc-button/macro.test.js
+++ app/components/general/components/bc-button/macro.test.js
@@ -19,4 +19,70 @@ describe('bcButton', () => {
       expect($('div[data-test-id="some-button-identifier"]').length).toEqual(1);
     });
   }));
+
+  it('should render the name of the button with the text provided', componentTester(setup, (harness) => {
+    const context = {
+      params: {
+        dataTestId: 'some-button-identifier',
+        enabled: true,
+        text: 'name of button',
+      },
+    };
+
+    harness.request(context, ($) => {
+      expect($('div[data-test-id="some-button-identifier"]').text().trim()).toEqual('name of button');
+    });
+  }));
+
+  it('should render with any classes provided', componentTester(setup, (harness) => {
+    const context = {
+      params: {
+        dataTestId: 'some-button-identifier',
+        enabled: true,
+        classes: 'some-class',
+      },
+    };
+
+    harness.request(context, ($) => {
+      expect($('div[data-test-id="some-button-identifier"] button').hasClass('some-class')).toEqual(true);
+    });
+  }));
+
+  it('should render the href link if provided', componentTester(setup, (harness) => {
+    const context = {
+      params: {
+        dataTestId: 'some-button-identifier',
+        href: '/some-href',
+      },
+    };
+
+    harness.request(context, ($) => {
+      expect($('div[data-test-id="some-button-identifier"] a').attr('href')).toEqual('/some-href');
+    });
+  }));
+
+  it('should not render the button as disabled if "disabled" flag is not provided', componentTester(setup, (harness) => {
+    const context = {
+      params: {
+        dataTestId: 'some-button-identifier',
+      },
+    };
+
+    harness.request(context, ($) => {
+      expect($('div[data-test-id="some-button-identifier"] button').hasClass('nhsuk-button--disabled')).toEqual(false);
+    });
+  }));
+
+  it('should render the button as disabled if "disabled" flag is provided', componentTester(setup, (harness) => {
+    const context = {
+      params: {
+        dataTestId: 'some-button-identifier',
+        disabled: true,
+      },
+    };
+
+    harness.request(context, ($) => {
+      expect($('div[data-test-id="some-button-identifier"] button').hasClass('nhsuk-button--disabled')).toEqual(true);
+    });
+  }));
 });

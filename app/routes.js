8cadea99714135da3e33e3e7082db9602d715c98
bjss-chris <59572143+bjss-chris@users.noreply.github.com>
2021-05-10 16:09:45 +0100

    Address uncontrolled data in path expression flaws (#298)
    AB#14520

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import express from 'express';
+import { sanitize } from 'sanitize-filename';
 import { generateTemplate } from './helpers/templateGenerator';
 
 const router = express.Router();
@@ -16,19 +17,19 @@ router.get('/holding', async (req, res) => {
 
 router.get('/:templateType', async (req, res) => {
   const { templateType } = req.params;
-  res.render(`componentLibraryViewer/${templateType}`);
+  res.render(`componentLibraryViewer/${sanitize(templateType)}`);
 });
 
 router.get('/components/:componentType', async (req, res) => {
   const { componentType } = req.params;
-  res.render(`componentLibraryViewer/components/${componentType}Components`);
+  res.render(`componentLibraryViewer/components/${sanitize(componentType)}Components`);
 });
 
 router.get('/components/:componentType/:component', async (req, res) => {
   const { component: name, componentType } = req.params;
   generateTemplate({ name, templateType: 'component', componentType });
 
-  res.render(`templates/components/${componentType}/components/${name}-template`);
+  res.render(`templates/components/${sanitize(componentType)}/components/${sanitize(name)}-template`);
 });
 
 router.post('/components/:componentType/:component', async (req, res) => {
@@ -41,21 +42,21 @@ router.post('/components/:componentType/:component', async (req, res) => {
     componentType,
   });
 
-  res.render(`templates/components/${componentType}/components/${name}-template`);
+  res.render(`templates/components/${sanitize(componentType)}/components/${sanitize(name)}-template`);
 });
 
 router.get('/sections/:sectionName', async (req, res) => {
   const name = req.params.sectionName;
   generateTemplate({ name, templateType: 'section' });
 
-  res.render(`templates/sections/${name}-template`);
+  res.render(`templates/sections/${sanitize(name)}-template`);
 });
 
 router.post('/sections/:sectionName', async (req, res) => {
   const name = req.params.sectionName;
   const formParams = JSON.parse(req.body.params);
   generateTemplate({ name, formParams, templateType: 'section' });
-  res.render(`templates/sections/${name}-template`, {});
+  res.render(`templates/sections/${sanitize(name)}-template`, {});
 });
 
 module.exports = router;
